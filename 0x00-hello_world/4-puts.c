fbe9aa75521e8f0c496c2b16c7f4869bb0303334
TianaGovender <tianagovender@gmail.com>
2022-06-02 12:30:48 -0700

    Puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry piont
+ * Return:1
+ */
 int main(void)
 {
-char str1[150] = "Programming is like building a multilingual puzzle";
+
+char str1[150] = "\"Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
+
 }
