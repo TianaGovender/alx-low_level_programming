e0d220492c8ac3c6930b7aec0265f970b2aadf33
TianaGovender <tianagovender@gmail.com>
2022-06-07 15:03:02 -0700

    Check

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,18 +18,18 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		putchar('+');
 		check = 1;
+		putchar('+');
 	}
 	if (n < 0)
 	{
+		check = -1;
 		putchar('-');
-		check = 1;
 	}
 	if (n == 0)
 	{
-		putchar(0 + '0');
 		check = 0;
+		putchar(0 + '0');
 	}
 	return (check);
 
