5b6cd934788a91193db07ce0dca8890ad5f85236
TianaGovender <tianagovender@gmail.com>
2022-06-07 13:21:05 -0700

    Is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: is an integer
+ *
+ * Return: 1 if n is greater than 0
+ * returns 0 if n is less than 0
+ */
+int _islower(int c)
+{
+	int n;
+
+	n = islower(c);
+
+	if (n > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
