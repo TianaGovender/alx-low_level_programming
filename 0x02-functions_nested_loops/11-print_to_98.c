b519573feaa76d015d57098614add06d23716e75
TianaGovender <tianagovender@gmail.com>
2022-06-07 19:02:06 -0700

    From n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,10 +15,30 @@ void print_to_98(int n)
 
 	while (i < 99)
 	{
-		if (i > 0)
+		if (i == 98)
+		{
+			printf("%d, ", i);
+		}
+		if ((i > 0) && (i != 98))
 		{
 			printf("%d, ", i);
 		}
 		i++;
+	}
+
+	if (i > 99)
+	{
+		while (i >= 98)
+		{
+			if (i == 98)
+			{
+				printf("%d, ", i);
+			}
+			if ((i > 0) && (i != 98))
+			{
+				printf("%d, ", i);
+			}
+			i--;
+		}
 	}
 }
