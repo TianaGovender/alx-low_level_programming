2fdb7a15f7de80e70391c0aed66e1764971166b3
TianaGovender <tianagovender@gmail.com>
2022-06-09 13:45:20 -0700

    Fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - fizz buzz fizzbuzz
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, num1, num2;
+
+	i = 0;
+	while (i <= 100)
+	{
+		num1 = i % 3;
+		num2 = i % 5;
+
+		if (num1 == 0 && num2 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			if (num1 == 0)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+				if (num2 == 0)
+				{
+					printf("Buzz ");
+				}
+				else
+				{
+					printf("%d ", i);
+				}
+			}
+		}
+		i++;
+	}
+
+	return (0);
+}
