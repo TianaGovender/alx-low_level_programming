2e6e4d0829e6e9086cbc7f4888a6b2675ad0879b
TianaGovender <tianagovender@gmail.com>
2022-06-13 08:07:32 -0700

    Replaced to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * reset_to_98 -resets value to 98
+ * @n: integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
