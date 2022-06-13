13ca8452f54ee2f846b7cfdfeb7bacd543677f34
TianaGovender <tianagovender@gmail.com>
2022-06-13 11:39:24 -0700

    ocnvert strint to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _atoi - convert string to int
+ * @s: char
+ * Return: string as integer value
+ */
+int _atoi(char *s)
+{
+	int i;
+
+	i = atoi(s);
+
+	return (i);
+}
