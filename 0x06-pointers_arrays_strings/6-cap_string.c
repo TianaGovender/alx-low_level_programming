4c66cc01d16cbea3f0b15476bedb378002b5db4b
TianaGovender <tianagovender@gmail.com>
2022-06-16 07:34:34 -0700

    Capitilize words in strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * cap_string - capitilizes words
+ * @str: char
+ * Return: capitilized string
+ */
+
+char *cap_string(char *str)
+{
+	int len1, i;
+
+	len1 = strlen(str);
+
+	i = 0;
+
+	while (i < len1)
+	{
+		if (str[i] == ' ')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = toupper(str[i + 1]);
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}
