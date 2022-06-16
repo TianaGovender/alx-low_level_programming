af5e6ecd11ace30847fad3cc1526e422544c73c5
TianaGovender <tianagovender@gmail.com>
2022-06-16 06:09:04 -0700

    Test

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies two strings
+ * @dest: char
+ * @src: char
+ * @n: integer
+ * Return: a copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len1, i;
+
+	len1 = strlen(dest);
+
+	i = 0;
+
+	while (i <= n)
+	{
+		dest[len1 + i] = src[i];
+		i++;
+	}
+
+
+	/*dest[len1 + i] = '\0';*/
+
+	return (dest);
+}
