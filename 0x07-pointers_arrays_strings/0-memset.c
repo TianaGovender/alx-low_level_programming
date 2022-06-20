bf0e0573120893d9f11f61686e2b4e17f1ed9066
TianaGovender <tianagovender@gmail.com>
2022-06-20 07:04:47 -0700

    Test

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memset - fills bytes of memory
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i, j, p, num;
+
+	j = sizeof(*s) / sizeof(s[0]);
+	i = 0;
+
+	while (i < j)
+	{
+		if (s[i] == *s)
+		{
+			num = n + i + 1;
+			p = i + 1;
+			while (p < num)
+			{
+				s[p] = b;
+				p++;
+
+			}
+
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+	return (s);
+}
