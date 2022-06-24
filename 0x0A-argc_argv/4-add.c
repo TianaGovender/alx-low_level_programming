7bcd39ff4db17742621e90780a40f3e9b9c513f9
TianaGovender <tianagovender@gmail.com>
2022-06-24 07:25:17 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,20 +16,16 @@ int main(int argc, char *argv[])
 	int i, j, sum;
 
 	sum = 0;
-	j = 0;
-
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
 
 	for (i = 1; i < argc; i++)
 	{
-		j = isdigit(argv[i]);
-		if (j == 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
 		sum = sum + atoi(argv[i]);
