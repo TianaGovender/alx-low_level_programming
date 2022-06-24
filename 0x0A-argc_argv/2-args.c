58c0f9532fe35b12e1917cdbedbb3cd61de282eb
TianaGovender <tianagovender@gmail.com>
2022-06-24 06:51:04 -0700

    Test

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
