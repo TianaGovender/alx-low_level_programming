33f57eee5f5459c548f7cf1ee3ae00fe4e7e92f8
TianaGovender <tianagovender@gmail.com>
2022-06-24 06:42:47 -0700

    Num of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
