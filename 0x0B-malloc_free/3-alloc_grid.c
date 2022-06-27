fc2bb4e48e33de3f9bf7e70eb9609145ea84f6b5
TianaGovender <tianagovender@gmail.com>
2022-06-27 10:26:19 -0700

    Task complete

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int *) * width);
+		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
 			for (i = i - 1; i >= 0; i--)
