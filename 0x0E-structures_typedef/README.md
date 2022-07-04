c5d41154c1acf76bbc866b216fec16260ee3cd19
TianaGovender <tianagovender@gmail.com>
2022-07-04 05:27:48 -0700

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Project on structs and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - info on dog
+ * @name: char
+ * @owner: char
+ * @age: float
+ *
+ * Description: struct containing info on dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
