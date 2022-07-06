86d47faa6dba6f9d976eaac08e40eda9bd86ffad
TianaGovender <tianagovender@gmail.com>
2022-07-06 04:48:49 -0700

    array with another function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - performs functions on each element of an array
+ * @array: int
+ * @size: size of array
+ * @action: pointer to print function
+ * Return: if not name or function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
