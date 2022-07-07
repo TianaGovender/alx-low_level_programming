daa2a6cf02392c8732bcaed463f8d0355615c8ec
TianaGovender <tianagovender@gmail.com>
2022-07-07 06:26:25 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", x);
 		if (separator != NULL)
 		{
-			printf("%s ", separator);
+			if (i != (n - 1))
+				printf("%s", separator);
 		}
 	}
 
