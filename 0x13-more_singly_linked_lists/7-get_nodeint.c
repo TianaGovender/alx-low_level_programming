ab3fb4f6774e24cd0af8984ede3e23d4b93ec718
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-07-19 17:42:29 +0200

    Task 7 complete

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,18 +3,25 @@
 #include "lists.h"
 
 /**
- * free_listint - fress list
- * @head: a pointer
+ * get_nodeint_at_index - Locates a given node of
+ *                        a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ * @index: The index of the node to locate - indices start at 0.
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the located node.
  */
-
-void free_listint(listint_t *head)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp;
+	unsigned int node;
 
-	while (head)
+	for (node = 0; node < index; node++)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
 	}
+
+	return (head);
 }
