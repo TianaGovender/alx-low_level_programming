2bcbbf75554bb74be7dad67f93a34ab21971230b
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-07-21 17:33:19 +0200

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,33 +3,24 @@
 #include "main.h"
 
 /**
- * print_binary - prints integers as binary
- * @n: long int
+ * flip_bits - Returns the number of bits you would need to flip to get
+ * from one number to another.
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: the number of bits you would need to flip to get
  */
-
-void print_binary(unsigned long int n)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (n == 0)
-		putchar('0');
-	else
-		binary_p(n);
-}
+	unsigned long int r, i;
+	
+	r = n ^ m;
+	i = 0;
+	while (r > 0)
+	{
+		if ((r & 1) == 1)
+			i++;
+		r = r >> 1;
+	}
 
-/**
- * binary_p - prints bin
- * @n: long int
- */
-
-void binary_p(unsigned long int n)
-{
-	if (n == 0)
-		return;
-
-	binary_p((n >> 1));
-
-	if ((n & 1) == 1)
-		putchar('1');
-
-	if ((n & 1) == 0)
-		putchar('0');
+	return (i);
 }
