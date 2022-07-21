177cd7989e7060a00e4b0a0f391f0dbebe55b12f
TianaGovender <tianagovender1@gmail.com>
2022-07-21 08:05:25 -0700

    Test task

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - prints integers as binary
+ * @n: long int
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		putchar('0');
+	else
+		binary_p(n);
+}
+
+/**
+ * binary_p - prints bin
+ * @n: long int
+ */
+
+void binary_p(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+	binary_p((n >> 1));
+
+	if ((n & 1) == 1)
+		putchar('1');
+
+	if ((n & 1) == 0)
+		putchar('0');
+}
