4957ca97f1edcd7fe8851b18c91b14e6e6e6d63c
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-07-21 16:54:45 +0200

    check task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,33 +9,27 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int bb, ANS = 0;
-	int i;
+	if (n == 0)
+		putchar('0');
+	else
+		binary_p(n);
+}
 
-	if (b == NULL)
-		return (0);
+/**
+ * binary_p - prints bin
+ * @n: long int
+ */
 
-	bb = 0;
-	i = 0;
-
-	while (b[i] != '\0')
-	{
-		if (b[i] == ' ')
-			return (0);
-
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-
-		if (b[i] == '0')
-			bb  = 0;
-		if (b[i] == '1')
-			bb = 1;
-
-		ANS = (2 * ANS) + bb;
-		i++;
-	}
-
-	return (ANS);
+void binary_p(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	
+	binary_p((n >> 1));
+	
+	if ((n & 1) == 1)
+		putchar('1');
+	
+	if ((n & 1) == 0)
+		putchar('0');
 }
