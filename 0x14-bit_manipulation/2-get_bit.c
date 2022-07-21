256f9c7250afd919ec14de9209eef082e5c4c423
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-07-21 17:11:18 +0200

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,33 +3,24 @@
 #include "main.h"
 
 /**
- * print_binary - prints integers as binary
- * @n: long int
+ * get_bit - Returns a value at a given index
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: A value at a given index or -1 if an error occours
  */
-
-void print_binary(unsigned long int n)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n == 0)
-		putchar('0');
-	else
-		binary_p(n);
-}
+	unsigned int i;
 
-/**
- * binary_p - prints bin
- * @n: long int
- */
-
-void binary_p(unsigned long int n)
-{
-	if (n == 0)
-		return;
-
-	binary_p((n >> 1));
-
-	if ((n & 1) == 1)
-		putchar('1');
-
-	if ((n & 1) == 0)
-		putchar('0');
+	if (index > (sizeof(8) * 8))
+		return (-1);
+	
+	i = 0; 
+	while (i < index)
+	{
+		n = n >> 1;
+		i++;
+	}
+	
+	return ((n & 1));
 }
