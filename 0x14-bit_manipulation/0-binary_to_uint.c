6f8bcc09b8d8067140a826ef5c524d04e1f02831
TianaGovender <tianagovender1@gmail.com>
2022-07-21 07:21:20 -0700

    Test Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int bb, ANS = 0;
-	int i = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
+
+	bb = 0;
+	i = 0;
 
 	while (b[i] != '\0')
 	{
 		if (b[i] == ' ')
 			return (0);
 
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
 
 		if (b[i] == '0')
 			bb  = 0;
