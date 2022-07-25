b3c6f532873bc0607c7951b74b57e1cc3537665a
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-07-25 19:30:15 +0200

    checking task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,52 +6,34 @@
 #include <fcntl.h>
 
 /**
- * read_textfile - reads text file and prints it
+ * append_text_to_file - Appends text at the end of a file.
  * @filename: char
- * @letters: size
- * Return: size or 0
+ * @text_content: String
+ * Return: 1 or -1
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, sz, i;
-	char *buf;
-
+	int file, i;
+	
 	if (filename == NULL)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (0);
-
-	file = open(filename, O_RDONLY);
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	
+	file = open(filename, O_APPEND | O_WRONLY);
 	if (file == -1)
-	{
-		free(buf);
-		return (0);
-	}
-
-	sz = read(file, buf, letters);
-	if (sz == -1)
-	{
-		close(file);
-		free(buf);
-		return (0);
-	}
-
+		return (-1);
 	i = 0;
-	while (i < sz)
+	while (text_content[i] != '\0')
 	{
-		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
+		if (write(file, &text_content[i], 1) == -1)
 		{
 			close(file);
-			free(buf);
-			return (0);
+			return (-1);
 		}
 		i++;
 	}
-
-	free(buf);
 	close(file);
-	return (sz);
+	return (1);
 }
