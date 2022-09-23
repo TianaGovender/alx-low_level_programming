a4f884111245d2730396d2c8c1cb130a1156755a
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-09-23 20:07:04 +0200

    Task 3 recheck

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,24 +1,76 @@
 #include "hash_tables.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table
+ * @key: is the key. key can not be an empty string
+ * @value: value associated with the key.
+ * value must be duplicated. value can be an empty string
+ * Return: 1 on success, 0 on failurre
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+hash_node_t *node;
+hash_node_t *new_node;
+unsigned long int index;
+
+if (ht == NULL || *key == '\n' || *value == '\n')
+	return (0);
+
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+
+if (node == NULL)
+{
+	new_node = create_new_node(key, value);
+	if (new_node == NULL)
+		return (0);
+
+	ht->array[index] = new_node;
+	return (1);
+}
+
+/*If key exists, replace value*/
+while (node != NULL)
+{
+	if (strcmp(key, node->key) == 0)
+	{
+		free(node->value);
+		node->value = strdup(value);
+		return (1);
+	}
+	node = node->next;
+}
+/*If key doesn't exist, create new node*/
+new_node = create_new_node(key, value);
+if (new_node == NULL)
+	return (0);
+
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+return (1);
+}
 
 /**
- * hash_djb2 - implements the djb2 algo
- * @str: string
- * Return: hash value
+ * create_new_node - create a new node
+ * @key: is the key. key can not be an empty string
+ * @value: value associated with the key.
+ * value must be duplicated. value can be an empty string
+ * Return: 1 on success, 0 on failurre
  */
 
-unsigned long int hash_djb2(const unsigned char *str)
+hash_node_t *create_new_node (const char *key, const char *value)
 {
-	unsigned long int h;
-	int i;
+	hash_node_t *new_node;
 
-	h = 5381;
-	while ((i = *str++))
-	{
-		h = ((h << 5) + h) + i;
-	}
+	new_node = malloc(sizeof(hash_node_t));
 
-	return (h);
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	return (new_node);
 }
