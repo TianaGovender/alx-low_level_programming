dda77e9391c8aba7769a9a17449ccb3ab02bba10
TianaGovender <tianagovender1@gmail.com>
2022-09-23 10:27:46 -0700

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ *hash_table_create - creates a hash table
+ * @size: long int that is size of hash table 
+ */
+
+hash_table_t *hash_table_create(unsigned long int size);
+{
+}
